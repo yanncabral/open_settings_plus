6a5682d8615e3cb2dda6a29809df37672fefccde
Yann <iamyanndias@gmail.com>
2024-01-31 16:07:35 -0300

    Fix docstring typos.

--- lib/core/open_settings_plus_core.dart
+++ lib/core/open_settings_plus_core.dart
@@ -2,12 +2,12 @@ import 'package:open_settings_plus/android/open_settings_plus_android.dart';
 import 'package:open_settings_plus/bridge/open_settings_plus_platform_interface.dart';
 import 'package:open_settings_plus/ios/open_settings_plus_ios.dart';
 
-// {@template open_settings_plus}
+/// {@template open_settings_plus_core}
 /// Singleton used to create a [OpenSettingsPlus] instance.
 /// You can get iOS or android instance of [OpenSettingsPlus] using this class.
 /// {@endtemplate}
 abstract class OpenSettingsPlus {
-  /// {@macro open_settings_plus}
+  /// {@macro open_settings_plus_core}
   const OpenSettingsPlus();
 
   /// Returns a singleton instance of [OpenSettingsPlus] for iOS.
